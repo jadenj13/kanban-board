b5d51d44ae28fa815338c814fedaab84f52374af
jadenj133 <jadenj133@gmail.com>
2020-01-26 14:04:27 -0700

    updates use local storage hook

--- src/hooks/use-local-storage.js
+++ src/hooks/use-local-storage.js
@@ -2,22 +2,13 @@ import { useState } from 'react';
 
 function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key);
+    const item = JSON.parse(window.localStorage.getItem(key));
 
-      if (item && typeof initialValue === 'string') {
-        return item;
-      }
-
-      if (Array.isArray(initialValue)) {
-        return item ? item.split(';') : initialValue;
-      }
-
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      console.log(error);
-      return initialValue;
+    if (item) {
+      return item.value;
     }
+
+    return initialValue;
   });
 
   const setValue = value => {
@@ -25,11 +16,7 @@ function useLocalStorage(key, initialValue) {
 
     setStoredValue(valueToStore);
 
-    if (Array.isArray(value)) {
-      window.localStorage.setItem(key, value.join(';'));
-    } else {
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
-    }
+    window.localStorage.setItem(key, JSON.stringify({ value: valueToStore }));
   };
 
   return [storedValue, setValue];
